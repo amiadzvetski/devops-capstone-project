2cbb53fdab307a2a448c65835bff737a027e8d6b
amiadzvetski <99020356+amiadzvetski@users.noreply.github.com>
2022-11-12 20:13:48 +0300

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,5 +135,5 @@ class TestAccountService(TestCase):
        
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
-        # send a self.client.get() request to the BASE_URL with an invalid account number (e.g., 0)
-        # assert that the resp.status_code is status.HTTP_404_NOT_FOUND
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
